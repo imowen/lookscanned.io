916d7a85c8c4aa0ec7acc7547417ce7fdb5de2ea
Owen <31156070+imowen@users.noreply.github.com>
2024-10-02 22:39:01 +0700

    Create AdComponent.vue

---
+++ src/views/AdComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ad-component">
+    <!-- 在这里放置您的广告代码 -->
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5638905191417408"
+         crossorigin="anonymous"></script>
+    <!-- make PDF like scanned -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-5638905191417408"
+         data-ad-slot="8775002895"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// 可能需要的任何广告相关逻辑
+</script>
+
+<style scoped>
+.ad-component {
+  /* 广告组件的样式 */
+}
+</style>
