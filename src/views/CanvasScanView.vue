55a312de047a455280c853256d6bf36fefe2d7c7
Owen <31156070+imowen@users.noreply.github.com>
2025-06-25 14:28:25 +0700

    更新 CanvasScanView.vue

--- src/views/CanvasScanView.vue
+++ src/views/CanvasScanView.vue
@@ -1,8 +1,6 @@
 <template>
   <MainContainer>
-    <div style="margin-bottom: 25px">
-      <BackToIndex />
-    </div>
+   
     <n-grid x-gap="25" y-gap="25" :cols="12" item-responsive responsive="screen">
       <n-grid-item span="12 s:5 m:4 l:3">
         <n-space vertical>
