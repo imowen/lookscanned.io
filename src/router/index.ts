9b5dff44c078b6454e426ab4de919718eed0c320
Owen <31156070+imowen@users.noreply.github.com>
2025-06-25 14:38:35 +0700

    更新 index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import IndexView from '@/views/IndexView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'index',
-      component: () => import('@/views/ScanViewFeatureDetectView.vue')
-    },
-    {
-      path: '/scan',
       name: 'scan',
       component: () => import('@/views/ScanViewFeatureDetectView.vue')
     },
@@ -28,7 +22,7 @@ const router = createRouter({
     {
       path: '/:pathMatch(.*)*',
       name: 'catch-all',
-      redirect: { name: 'index' }
+      redirect: { name: 'scan' }
     }
   ]
 })
